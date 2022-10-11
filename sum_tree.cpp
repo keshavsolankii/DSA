2ad69982968e29bfcd2c5637599ee12e166c9dde
keshavsolankii <110564015+keshavsolankii@users.noreply.github.com>
2022-10-11 21:41:01 +0530

    Create sum_tree.cpp

---
+++ sum_tree.cpp
@@ -0,0 +1,34 @@
+pair<bool, int> isSumTree(Node* root)
+{
+  if(root == NULL)
+  {
+    pair<bool, int> p = make_pair(NULL, 0);
+    return p;
+  }
+  if(root -> left == NULL && root -> right == NULL)
+  {
+    pair<bool, int> p = make_pair(true, root->data);
+    return p;
+  }
+  
+  pair<bool, int> left = isSumTree(root->left);
+  pair<bool, int> right = isSumTree(root->right);
+  
+  bool leftAns = left.first;
+  bool.rightAns = right.first;
+  
+  bool val = (root->data == left.second + right.second);
+  
+  pair<bool, int> ans;
+  
+  if(leftAns && rightAns && val)
+  {
+    ans.first = true;
+    ans.second = 2*root->data;
+  }
+  else
+  {
+    ans.first = false;
+  }
+  return ans;
+}
