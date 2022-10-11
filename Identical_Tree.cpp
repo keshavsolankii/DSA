1562d5a87435ecbdc82283e9c349af0ef97d99b9
keshavsolankii <110564015+keshavsolankii@users.noreply.github.com>
2022-10-11 20:58:29 +0530

    Create Identical_Tree.cpp

---
+++ Identical_Tree.cpp
@@ -0,0 +1,21 @@
+// Let root of tree 1 = r1 and root of tree 2 = r2
+
+bool identicalTrees(Node* r1, Node* r2)
+{
+  if(r1 == NULL && r2 == NULL)
+    return true;
+  if(r1 == NULL && r2 != NULL)
+    return false;
+  if(r1 != NULL && r2 == NULL)
+    return false;
+  
+  bool left = identicalTrees(r1->left, r2->left);
+  bool right = identicalTrees(r1->right, r2->right);
+  
+  bool ans = (r1->data == r2->data);
+  
+  if(left && right && ans)
+    return true;
+  else
+    return false;
+}
